f4ac80087a78b733c94e6342b9901b25686f339c
taba2177 <tabadev217@gmail.com>
2024-10-21 19:41:29 +0300

    #

--- App.js
+++ App.js
@@ -74,7 +74,7 @@ const App = () => {
 
   // Send the ZPL command to the printer
   const sendToPrinter = (zplCommandString) => {
-    const client = Net.createConnection({ host: '192.168.8.135', port: 9100 }, () => {
+    const client = Net.createConnection({ host: '192.168.1.100#', port: 9100 }, () => {
       Alert.alert(zplCommandString);
       client.write(zplCommandString, 'utf-8');
       client.end();
